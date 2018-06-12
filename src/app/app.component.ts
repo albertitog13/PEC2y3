b9edf38942cd4a2e5c33713cad25368052259764
Nombre que quieras mostrarAlberto <albertitog13@gmail.com>
2018-06-12 18:03:04 +0200

    Practica 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Alberto';
+  private chartData: Array<any>;
+
+  // constructor(private _ApiService: ApiService) { }
+
+  ngOnInit() {
+
+    // Local Data //////////////////////////////////////////////////////////////////////
+    this.generateData();
+    // change the data periodically
+    //setInterval(() => this.generateData(), 3000);
+    // API Data ////////////////////////////////////////////////////////////////////////
+    // this._ApiService.getData().subscribe(result => { this.chartData = result })
+  }
+
+  generateData() {
+    this.chartData = [];
+    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
+      this.chartData.push([
+        `Index ${i}`,
+        Math.floor(Math.random() * 100)
+      ]);
+    }
+  }
 }
